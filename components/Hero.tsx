e99ff4ddf7ab56d91b158fc89b0d9a915e60a23a
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-25 16:16:31 -0400

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { Spotlight } from "./ui/Spotlight";
 
 const Hero = () => {
   return (
     <>
-      <div className="pb-20 pt-36"></div>
+      <div className="pb-20 pt-36">
+        <div>
+          <Spotlight
+            className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+            fill="white"
+          />
+          <Spotlight
+            className="-top-10 -left-full h-[80vh] w-[50vw]"
+            fill="purple"
+          />
+          <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+        </div>
+        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+        </div>
+        <div className="flex justify-center ">
+          <div className="max-w-[88vw]">
+            <h1>Dynamic Web Magic with Next.js</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
