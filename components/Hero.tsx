0c1811e320f1db2de6d9d673267fdf7558e94eca
utsavpatel562 <utsavpatel562@gmail.com>
2024-06-25 10:59:52 -0400

    Initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <div className="pb-20 pt-36"></div>
+    </>
+  );
+};
+
+export default Hero;
---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+import { DEFAULT_CIPHERS } from "tls";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        black:{
+          DEFAULT: "#000",
+          100: "#000319",
+        }
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
